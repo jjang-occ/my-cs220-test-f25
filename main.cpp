45c9a19e7484345eba4417c1eca62809ae2f9df4
Justin J <jjang@occ.cccd.edu>
2025-11-06 13:05:25 -0800

    string stuff

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 //Hi
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     cout << "hello world" << endl;
     cout << "This is fun!" << endl;
+
+    string s = "abc";
+    cout << s << endl;
 }
